fd2955cdd8064bf53af7ba8ebca41ec0aed12e51
lianghaowei <372024070@qq.com>
2016-12-27 09:09:00 +0800

    use before_action to find_group_and_check_permission

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -14,11 +14,7 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
-
-    if current_user != @group.user
-      redirect_to root_path, alert: "You have no permission."
-    end
+    find_group_and_check_permission
   end
 
   def create
@@ -32,12 +28,9 @@ class GroupsController < ApplicationController
     end
   end
 
-  def update
-    @group = Group.find(params[:id])
-
-    if current_user != @group.user
-      redirect_to root_path, alert: "You have no permission."
-    end
+  def edit
+    find_group_and_check_permission
+  end
 
     if @group.update(group_params)
       redirect_to groups_path, notice: "Update Success"
@@ -46,18 +39,23 @@ class GroupsController < ApplicationController
     end
   end
 
-  def destroy
-    @group = Group.find(params[:id])
-
-    if current_user != @group.user
-      redirect_to root_path, alert: "You have no permission."
-    end
+  def edit
+    find_group_and_check_permission
+  end
 
     @group.destroy
     redirect_to groups_path, alert: "Group deleted"
   end
 
   private
+
+  def find_group_and_check_permission
+    @group = Group.find(params[:id])
+
+    if current_user != @group.user
+      redirect_to root_path, alert: "You have no permission."
+    end
+  end
 
   def group_params
     params.require(:group).permit(:title, :description)
